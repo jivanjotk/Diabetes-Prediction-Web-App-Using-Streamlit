d7156dc990312d7f66f4845db20cb161d1a8b902
Jivanjot Kaur <122741477+jivanjotk@users.noreply.github.com>
2025-01-10 11:09:20 +0530

    Update predictive system.py

--- predictive system.py
+++ predictive system.py
@@ -14,7 +14,7 @@ input_data = (5,166,72,19,175,25.8,0.587,51)
 
 input_data_as_array = np.asarray(input_data)
 
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+input_data_reshaped = input_data_as_array.reshape(1,-1)
 
 prediction = loaded_model.predict(input_data_reshaped)
 print(prediction)
